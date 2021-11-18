209ca2327ea8d7a0b7cdd88a122025e23eebd3a2
加藤晟也 <katoseiya@katouakiranarinoMacBook.local>
2021-11-18 17:12:44 +0900

    first commit

--- admin/orders.php
+++ admin/orders.php
@@ -16,7 +16,7 @@
         $Ostatus = $_GET["Ostatus"];
         $email = $_GET["email"];
 
-        require "admin.php";
+        require "classes/admin.php";
         $admin = new Admin();
         $Dtime = $admin -> display_delivery();
         $Dstatus = $admin -> get_Dstatus();
